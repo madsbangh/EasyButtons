f9933d71f77b5b4b2cbb1fc6074dd7c721e2d79b
Mads Bang Hoffensetz <faetter.mads@hotmail.com>
2017-03-08 22:17:03 +0100

    Initial Commit

---
+++ ButtonAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EasyButtons
+{
+    /// <summary>
+    /// Attribute to create a button in the inspector for calling the method it is attached to.
+    /// The method must have no arguments.
+    /// </summary>
+    /// <example>
+    /// [<see cref="ButtonAttribute"/>]
+    /// void MyMethod()
+    /// {
+    ///     Debug.Log("Clicked!");
+    /// }
+    /// </example>
+    [AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    public sealed class ButtonAttribute : Attribute { }
+}
---
+++ Editor/ButtonEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Editor that adds Easy Buttons to all MonoBehaviour derived classes.
+/// </summary>
+namespace EasyButtons
+{
+    // Custom inspector for MonoBehaviour including derived classes
+    [CustomEditor(typeof(MonoBehaviour), true)]
+    public class ButtonEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            // Loop through all methods with the Button attribute and no arguments
+            foreach (var method in target.GetType().GetMethods()
+                .Where(m => m.GetCustomAttributes(typeof(ButtonAttribute), true).Length > 0)
+                .Where(m => m.GetParameters().Length == 0))
+            {
+                // Draws a button which invokes the method
+                if (GUILayout.Button(method.Name))
+                {
+                    method.Invoke(target, new object[0]);
+                }
+            }
+            // Draw the rest of the inspector as usual
+            DrawDefaultInspector();
+        }
+    }
+}
