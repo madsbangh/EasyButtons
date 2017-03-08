bf1e66efa159f636cb58e79fc2792225342d4498
Mads Bang Hoffensetz <faetter.mads@hotmail.com>
2017-03-08 22:17:03 +0100

    Initial Commit

---
+++ Assets/EasyButtons/ButtonsExample.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using EasyButtons;
+
+public class ButtonsExample : MonoBehaviour
+{
+    // Example use of the ButtonAttribute
+    [Button]
+    public void TestButton1()
+    {
+        Debug.Log("Yes! Please do it again :D");
+    }
+
+    [Button]
+    public void TestButton2()
+    {
+        Debug.Log("Ouch! Stop clicking me :(");
+    }
+}
