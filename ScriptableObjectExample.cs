78e929bde7d0e2b956ad88dc878e7f66da21c6e3
Mads Bang Hoffensetz <faetter.mads@hotmail.com>
2017-03-22 14:29:37 +0100

    Remove unnecessary usings in example script

--- ScriptableObjectExample.cs
+++ ScriptableObjectExample.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Random = UnityEngine.Random;
-using Object = UnityEngine.Object;
+﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "Example.asset", menuName = "New Example ScriptableObject")]
 public class ScriptableObjectExample : ScriptableObject
