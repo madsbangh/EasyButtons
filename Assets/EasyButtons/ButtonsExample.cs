df104680fb22bddc633e103a5530499b580a049e
Mads Bang Hoffensetz <faetter.mads@hotmail.com>
2017-04-01 03:34:58 +0200

    Refactor and rename

--- Assets/EasyButtons/ButtonsExample.cs
+++ Assets/EasyButtons/ButtonsExample.cs
@@ -11,14 +11,14 @@ public class ButtonsExample : MonoBehaviour
     }
 
     // Example use of the ButtonAttribute that is not shown in play mode
-    [Button(ShowMode.HideInPlayMode)]
+    [Button(ButtonMode.DisabledInPlayMode)]
     public void SayHelloEditor()
     {
         Debug.Log("Hello from edit mode");
     }
 
     // Example use of the ButtonAttribute that is only shown in play mode
-    [Button(ShowMode.PlayModeOnly)]
+    [Button(ButtonMode.EnabledInPlayMode)]
     public void SayHelloInRuntime()
     {
         Debug.Log("Hello from play mode");
