920474ca0442072dc41d199b76c16a1054549d16
SolidAlloy <perepelartem@gmail.com>
2020-12-05 11:50:04 +0200

    Removed the "no parameters" check for methods

--- Editor/EasyButtonsEditorExtensions.cs
+++ Editor/EasyButtonsEditorExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Reflection;
 using UnityEditor;
 using UnityEngine;
@@ -10,45 +9,61 @@ namespace EasyButtons
     {
         public static void DrawEasyButtons(this Editor editor)
         {
-            // Loop through all methods with no parameters
-            var methods = editor.target.GetType()
-                .GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)
-                .Where(m => m.GetParameters().Length == 0);
+            // Loop through all methods
+            var flags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+            var methods = editor.target.GetType().GetMethods(flags);
 
             foreach (var method in methods)
             {
-                // Get the ButtonAttribute on the method (if any)
-                var ba = (ButtonAttribute)Attribute.GetCustomAttribute(method, typeof(ButtonAttribute));
-
-                if (ba != null)
+                if (method.GetParameters().Length == 0)
                 {
-                    // Determine whether the button should be enabled based on its mode
-                    var wasEnabled = GUI.enabled;
-
-                    bool inAppropriateMode = EditorApplication.isPlaying
-                        ? ba.Mode == ButtonMode.EnabledInPlayMode
-                        : ba.Mode == ButtonMode.DisabledInPlayMode;
-
-                    GUI.enabled = ba.Mode == ButtonMode.AlwaysEnabled || inAppropriateMode;
-
-
-                    if (((int)ba.Spacing & (int)ButtonSpacing.Before) != 0) GUILayout.Space(10);
-
-                    // Draw a button which invokes the method
-                    var buttonName = string.IsNullOrEmpty(ba.Name) ? ObjectNames.NicifyVariableName(method.Name) : ba.Name;
-                    if (GUILayout.Button(buttonName))
-                    {
-                        foreach (var t in editor.targets)
-                        {
-                            method.Invoke(t, null);
-                        }
-                    }
-
-                    if (((int)ba.Spacing & (int)ButtonSpacing.After) != 0) GUILayout.Space(10);
-
-                    GUI.enabled = wasEnabled;
+                    DrawButtonWithoutParams(editor, method);
+                }
+                else
+                {
+                    DrawButtonWithParams(editor, method);
                 }
             }
+        }
+
+        private static void DrawButtonWithoutParams(Editor editor, MethodInfo method)
+        {
+            // Get the ButtonAttribute on the method (if any)
+            var ba = (ButtonAttribute)Attribute.GetCustomAttribute(method, typeof(ButtonAttribute));
+
+            if (ba != null)
+            {
+                // Determine whether the button should be enabled based on its mode
+                var wasEnabled = GUI.enabled;
+
+                bool inAppropriateMode = EditorApplication.isPlaying
+                    ? ba.Mode == ButtonMode.EnabledInPlayMode
+                    : ba.Mode == ButtonMode.DisabledInPlayMode;
+
+                GUI.enabled = ba.Mode == ButtonMode.AlwaysEnabled || inAppropriateMode;
+
+
+                if (((int)ba.Spacing & (int)ButtonSpacing.Before) != 0) GUILayout.Space(10);
+
+                // Draw a button which invokes the method
+                var buttonName = string.IsNullOrEmpty(ba.Name) ? ObjectNames.NicifyVariableName(method.Name) : ba.Name;
+                if (GUILayout.Button(buttonName))
+                {
+                    foreach (var t in editor.targets)
+                    {
+                        method.Invoke(t, null);
+                    }
+                }
+
+                if (((int)ba.Spacing & (int)ButtonSpacing.After) != 0) GUILayout.Space(10);
+
+                GUI.enabled = wasEnabled;
+            }
+        }
+
+        private static void DrawButtonWithParams(Editor editor, MethodInfo method)
+        {
+
         }
     }
 }
