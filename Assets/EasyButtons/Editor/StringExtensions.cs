885e34efd4fcd8b042a72ef1661ea46accca73cb
SolidAlloy <perepelartem@gmail.com>
2020-12-05 15:26:29 +0200

    Made ScriptableObjectGenerator return a cached type if one already was generated

---
+++ Assets/EasyButtons/Editor/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace EasyButtons
+{
+    using System;
+
+    public static class StringExtensions
+    {
+        public static string CapitalizeFirstChar(this string input)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+            
+            char firstChar = input[0];
+
+            if (char.IsUpper(firstChar))
+                return input;
+            
+            var chars = input.ToCharArray();
+            chars[0] = char.ToUpper(firstChar);
+            return new string(chars);
+        }
+    }
+}
