4d8aef37de3d2bba13a472f59b4fad44ac7328bc
SolidAlloy <perepelartem@gmail.com>
2020-12-07 11:19:24 +0200

    Renamed ParamInfo to Parameter and inverted if's.

--- Assets/EasyButtons/Editor/ButtonWithParams.cs
+++ Assets/EasyButtons/Editor/ButtonWithParams.cs
@@ -10,13 +10,13 @@
 
     internal class ButtonWithParams : Button
     {
-        private readonly ParamInfo[] _parameters;
+        private readonly Parameter[] _parameters;
         private bool _expanded;
 
         public ButtonWithParams(MethodInfo method, ButtonAttribute buttonAttribute, ParameterInfo[] parameters)
             : base(method, buttonAttribute)
         {
-            _parameters = parameters.Select(parameter => new ParamInfo(parameter)).ToArray();
+            _parameters = parameters.Select(paramInfo => new Parameter(paramInfo)).ToArray();
         }
 
         protected override void DrawInternal(IEnumerable<Object> targets)
@@ -25,30 +25,30 @@
 
             _expanded = DrawUtility.DrawInFoldout(foldoutRect, _expanded, Name, () =>
             {
-                foreach (ParamInfo param in _parameters)
+                foreach (Parameter param in _parameters)
                 {
                     param.Draw();
                 }
             });
 
-            if (GUI.Button(buttonRect, "Invoke"))
-            {
-                var paramValues = _parameters.Select(param => param.Value).ToArray();
+            if ( ! GUI.Button(buttonRect, "Invoke"))
+                return;
 
-                foreach (Object obj in targets)
-                {
-                    Method.Invoke(obj, paramValues);
-                }
+            var paramValues = _parameters.Select(param => param.Value).ToArray();
+
+            foreach (Object obj in targets)
+            {
+                Method.Invoke(obj, paramValues);
             }
         }
 
-        private readonly struct ParamInfo
+        private readonly struct Parameter
         {
             private readonly FieldInfo _fieldInfo;
             private readonly ScriptableObject _scriptableObj;
             private readonly Editor _editor;
 
-            public ParamInfo(ParameterInfo paramInfo)
+            public Parameter(ParameterInfo paramInfo)
             {
                 Type generatedType = ScriptableObjectCache.GetClass(paramInfo.Name, paramInfo.ParameterType);
                 _scriptableObj = ScriptableObject.CreateInstance(generatedType);
