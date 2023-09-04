544c9f832b2eadc01039c3059b5c161628c18952
Bullet <bartek.blachucki@gmail.com>
2023-09-04 20:21:13 +0200

    feat: default values handling

--- Editor/Utils/ScriptableObjectCache.cs
+++ Editor/Utils/ScriptableObjectCache.cs
@@ -15,42 +15,47 @@
 
         private static readonly AssemblyBuilder _assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(
             new AssemblyName(AssemblyName)
-        {
-            CultureInfo = CultureInfo.InvariantCulture,
-            Flags = AssemblyNameFlags.None,
-            ProcessorArchitecture = ProcessorArchitecture.MSIL,
-            VersionCompatibility = AssemblyVersionCompatibility.SameDomain
-        }, AssemblyBuilderAccess.Run);
+            {
+                CultureInfo = CultureInfo.InvariantCulture,
+                Flags = AssemblyNameFlags.None,
+                ProcessorArchitecture = ProcessorArchitecture.MSIL,
+                VersionCompatibility = AssemblyVersionCompatibility.SameDomain
+            }, AssemblyBuilderAccess.Run);
 
         private static readonly ModuleBuilder _moduleBuilder = _assemblyBuilder.DefineDynamicModule(AssemblyName, true);
 
         private static readonly Dictionary<string, Type> _classDict = new Dictionary<string, Type>();
 
-        public static Type GetClass(string fieldName, Type fieldType)
+        public static Type GetClass(string fieldName, Type fieldType, bool hasDefaultValue, object defaultValue)
         {
             string className = GetClassName(fieldName, fieldType);
 
             if (_classDict.TryGetValue(className, out Type classType))
                 return classType;
 
-            if ( ! fieldType.IsUnitySerializable())
+            if (!fieldType.IsUnitySerializable())
             {
                 fieldType = typeof(NonSerializedError);
             }
 
-            classType = CreateClass(className, fieldName, fieldType);
+            classType = CreateClass(className, fieldName, fieldType, hasDefaultValue, defaultValue);
             _classDict[className] = classType;
             return classType;
         }
 
-        private static Type CreateClass(string className, string fieldName, Type fieldType)
+        private static Type CreateClass(string className, string fieldName, Type fieldType, bool hasDefaultValue,
+            object defaultValue)
         {
             TypeBuilder typeBuilder = _moduleBuilder.DefineType(
                 $"{AssemblyName}.{className}",
                 TypeAttributes.NotPublic,
                 typeof(ScriptableObject));
 
-            typeBuilder.DefineField(fieldName, fieldType, FieldAttributes.Public);
+            var fieldBuilder = typeBuilder.DefineField(fieldName, fieldType, FieldAttributes.Public);
+
+            if (hasDefaultValue && defaultValue != null)
+                DefaultValuesUtility.InjectDefaultValue(typeBuilder, fieldBuilder, defaultValue);
+
             Type type = typeBuilder.CreateType();
             return type;
         }
